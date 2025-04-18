ef515b4f883b341596c343aa721783bc1d80db5e
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-04-18 08:47:34 +0300

    Add the background color for the header and the button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 // eslint-disable-next-line react/prop-types
-export default function Search({ handleSearch }) {
+export default function Search({ handleSearch, headerBg, buttonBg }) {
   const [city, setCity] = useState("Tripoli");
 
   useEffect(() => {
@@ -16,7 +16,7 @@ export default function Search({ handleSearch }) {
   };
 
   return (
-    <header className="bg-[#9ccef4] p-5 shadow-sm transition-colors duration-[3000ms]">
+    <header className={`p-5 shadow-sm transition-colors duration-[3000ms] ${headerBg}`}>
       <div className="container flex items-center justify-between md:justify-start gap-5">
         <input
           type="text"
@@ -24,12 +24,13 @@ export default function Search({ handleSearch }) {
           value={city}
           onKeyDown={handleKeyDown}
           onChange={(e) => setCity(e.target.value)}
-          className="text-lg md:text-xl max-w-[40%] py-2 outline-none border-b border-none focus:border-solid border-[#090c51] text-white bg-transparent"
+          className="text-lg md:text-xl max-w-[40%] py-2 outline-none border-b border-transparent focus:border-[#090c51] text-white transition-all duration-300"
           placeholder="Type in a city name"
         />
         <button
-          className="bg-[#5879c7] text-md md:text-lg text-[#171f66] py-2 px-5 rounded-sm tracking-tight cursor-pointer"
+          className={`text-md md:text-lg text-[#171f66] py-2 px-5 rounded-sm tracking-tight cursor-pointer transition-colors duration-[3000ms] ${buttonBg}`}
           onClick={() => handleSearch(city)}
+          data-key="btn_search"
         >
           FIND WEATHER
         </button>
